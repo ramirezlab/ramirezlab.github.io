92b65e51572363c2f5a3528a78060356ad176945
ramirezlab <47159651+ramirezlab@users.noreply.github.com>
2019-05-14 23:56:52 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,8 +1,14 @@
 <?php
 $Name = $_POST['Name']; 
-$Last name = $_POST['Last name']; 
+$Lastname = $_POST['Last name']; 
 $email= $_POST['email']; 
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+echo $Name. "has said: <br/>".$message;
+if (mail('david.ramirez@uautonoma.cl', $subject, $message)){
+  echo "email sent";
+}else{
+  echo ¨problem with the email";
+}
 ?>
